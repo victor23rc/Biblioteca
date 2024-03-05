698ae19ce461682e3387df7f50b3745134691030
Victor Campos <victor.campos@ninecorp.local>
2024-03-04 23:11:25 -0300

    commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const mongoose = require('mongoose');
+
+const app = express()
+app.use(express.json())
+const port = 30001
+mongoose.connect('mongodb+srv://23rcvictor:c48UxcEf6JCvuigV@bibilhotecadb.lobhoq6.mongodb.net/?retryWrites=true&w=majority&appName=BibilhotecaDB');
+
+const LivroModel = mongoose.model('Livro', { 
+    title: String,
+    autor: String,
+    type: String,
+    url_image: String,
+    quantidade: Number
+  });
+
+  app.get('/Teste', (req, res) => {
+    res.send('Onlineeee')
+  })
+
+
+app.get('/Biblioteca', async (req, res) => {
+    const livros  = await LivroModel.find();
+    res.send(livros);
+  })
+
+
+app.post('/', async (req, res) => {
+    const novoLivro = new LivroModel({
+        title: req.body.title,
+        autor: req.body.autor,
+        type: req.body.type,
+        url_image: req.body.url_image,
+        quantidade: req.body.quantidade
+      });
+
+      await novoLivro.save();
+      res.send(novoLivro);
+  
+  })
+  
+
+
+app.listen(port, () => {
+  console.log(`executando ${port}`)
+})
